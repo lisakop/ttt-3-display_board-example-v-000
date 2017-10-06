fed7ba038acda9f674af3750c002040fadd9aed9
Lisa Koperski <lisa_koperski_junk@yahoo.com>
2017-10-06 03:20:47 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,23 +1,50 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-display_board = ["   ",'|',"-----------",n]
+#display_board = ["   ",'|',"-----------",n]
 
 def display_board
-  puts "A Tic Tac Toe Board"
-  puts display_board[0]
-  puts display_board[1]
-  puts display_board[0]
-  puts display_board[1]
-  prints display_board[0]
-  prints display_board[2]
-  puts display_board[0]
-  puts display_board[1]
-  puts display_board[0]
-  puts display_board[1]
-  prints display_board[0]
-  prints display_board[2]
-  puts display_board[0]
-  puts display_board[1]
-  puts display_board[0]
-  puts display_board[1]
-  prints display_board[0]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
+#   puts "A Tic Tac Toe Board"
+#   puts display_board[0]
+#   puts display_board[1]
+#   puts display_board[0]
+#   puts display_board[1]
+#   prints display_board[0]
+#   prints display_board[2]
+#   puts display_board[0]
+#   puts display_board[1]
+#   puts display_board[0]
+#   puts display_board[1]
+#   prints display_board[0]
+#   prints display_board[2]
+#   puts display_board[0]
+#   puts display_board[1]
+#   puts display_board[0]
+#   puts display_board[1]
+#   prints display_board[0]
+# end
+#
+# // ♥ irb
+# 001:0 > def display_board
+# 002:1 >     puts "A Tic Tac Toe Board"
+# 003:1 >   end
+
+#1 GET the value of a variable
+# board
+# a
+
+#2 SET the value of a variable
+# board = ["stuff"]
+# a = 9
+
+#3 DEFINE the steps of a function
+#def some_method(ingredients)
+#  puts "huzzah!"
+#end
+
+#4 EXECUTE the steps of a function
+#some_method
